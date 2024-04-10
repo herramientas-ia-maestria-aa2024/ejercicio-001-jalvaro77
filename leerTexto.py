6a5523bec1500772bebe276fbf411d24e0c7d088
j.yupangui <j_alvaro_7@hotmail.com>
2024-04-09 22:36:41 -0500

    Desarrollo Ejercicio 001
    Se agrega "leerTexto.py" para leer archivo de texto e imprimir cada linea.

---
+++ leerTexto.py
@@ -0,0 +1,14 @@
+"""
+Leer informacion de un txt e imprimir cada liena
+
+Autor: Juan Yupangui
+Fecha: 09/04/2024
+"""
+# Abrir archivo modo lectura utf-8 para caracteres latinos
+with open('informacion.txt', 'r', encoding='utf-8') as archivo:
+    # leer lineas
+    lineas = archivo.readlines()
+
+# imprimir cada linea
+for numero_linea, linea in enumerate(lineas, start=1):
+    print(f'Línea {numero_linea}: {linea.strip()}')#impreme numero de lineas y texto de la linea
